6ddc53b3e41a450102f386c94637ce23545d7b50
Tim Benniks <tbenniks@gmail.com>
2024-11-29 18:50:23 +0100

    meh

--- app/full-page/page.tsx
+++ app/full-page/page.tsx
@@ -5,11 +5,9 @@ import { useAppLocation } from "@/hooks/useAppLocation";
 
 export default function Home() {
   const { sdk, isInitialized, appConfig, error } = useContentstack();
-
-  console.log({ sdk, isInitialized, appConfig, error });
-
   const { locationName, location } = useAppLocation();
 
+  console.log({ sdk, isInitialized, appConfig, error });
   console.log({ locationName, location });
 
   return <main>{locationName}</main>;
